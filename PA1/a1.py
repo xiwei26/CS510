69e43c40302fb017fbae094ef27114e78fe9a196
Wei Xi <xiweiboy@gmail.com>
2017-01-29 16:00:37 -0700

    modified from Jason's version
    Fixed some bugs

--- PA1/a1.py
+++ PA1/a1.py
@@ -2,88 +2,71 @@ import sys
 import numpy as np
 import cv2
 
-
-if len(sys.argv)!=3:
+#####input and output file directories and names#####
+if len(sys.argv) < 3:
 	print "not enought arguments"
-
+	sys.exit()
 in_file=sys.argv[1]
-out_file=sys.argv[2]
+print in_file
+out_file=sys.argv[2] + '.avi'
 
-
-
-
-
-
-
-cap = cv2.VideoCapture('/Users/xiwei/Desktop/pa1/example1.mov')
+#####read video and information#########
+cap = cv2.VideoCapture(in_file)
+#cap = cv2.VideoCapture('example1.mov')
 if cap.isOpened()==False:
-	print("Can not find the video")
-
+	print("Can not open the video")
+	sys.exit()
 #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 #out = cv2.VideoWriter('../out.avi',fourcc, 30.0, (1280,720))
+#out = cv2.VideoWriter('output.mov', -1, 30, (1280,720))
+total_frame_num = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+#print (total_duration)
+fps = int(cap.get(cv2.cv.CV_CAP_PROP_FPS))
+codec = int(cap.get(cv2.cv.CV_CAP_PROP_FOURCC))
+#print(fps)
 
-out = cv2.VideoWriter('output.mov', -1, 30, (1280,720))
-
-total_duration = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-print (total_duration)
-
-fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
-print(fps)
-
-
-
-start_frame = int(raw_input("Please tell me the start frame: "))
-end_frame = int(raw_input("Please tell me the last frame:"))
+####user input#######
+start_frame = int(raw_input("Please tell me the start frame (0-based): "))
+end_frame = int(raw_input("Please tell me the last frame (0-based): "))
 startX = int(raw_input("what's the object's start x: "))
 startY = int(raw_input("what's the object's start y: "))
 height = int(raw_input("what is the object's height: "))
 width = int(raw_input("what is the object's width: "))
-speed_in_x= int(raw_input("what's the velosity in x of the object: "))
-speed_in_y = int(raw_input("what's the velosity in y of the object: "))
+speed_in_x= float(raw_input("what's the velosity in x axis: ")) #can be double
+speed_in_y = float(raw_input("what's the velosity in y axis: ")) #can be double
+output_ht = int(raw_input("what is the output video's height: "))
+output_wd = int(raw_input("what is the output video's width: "))
 
-start_frame_no=(start_frame/(total_duration*fps))
-print(start_frame_no)
-end_frame_no=(end_frame/(total_duration*fps))
-print(end_frame_no)
+#start_frame_no=(start_frame/(total_duration*fps))
+#print(start_frame_no)
+#end_frame_no=(end_frame/(total_duration*fps))
+#print(end_frame_no)
 #cap.set(2, start_frame) 
 #totalframe=end_frame-start_frame
 #cap.set(7, duration) 
 
+out = cv2.VideoWriter(out_file, codec, fps, (output_wd,output_ht))
+if out.isOpened() == False:
+	print("Cannot open output file!")
+	sys.exit()
+
 cap.set(1, start_frame)
-
-for num in range (start_frame,end_frame):
-	ret,frame = cap.read()
-
+for i in range (start_frame,end_frame + 1):
+	currX = float(startX + speed_in_x * (i - start_frame) ) #Better this way if speed_in_x is a double
+	currY = float(startY + speed_in_y * (i - start_frame) ) 
+	ret, frame = cap.read()
 	#a = np.matrix('1 2; 3 4')
-
-
-	pts1 = np.float32([[startX,startY],[startX,startY+height],[startX+width,startY]])
-	pts2 = np.float32([[0,0],[0,720],[1280,0]])
+	pts1 = np.float32([[currX, currY],[currX, currY+height-1],[currX+width-1, currY]])
+	pts2 = np.float32([[0,0],[0,output_ht-1],[output_wd-1,0]])
 	M = cv2.getAffineTransform(pts1,pts2)
 	#cv2.rectangle(frame, (startX, startY), (startX + width, startY+ height), (0,0,255),2)
-
-
-	dst = cv2.warpAffine(frame,M,(1280,720))
-
-
-
-
-
-	
-	startX = startX + speed_in_x
-	startY = startY + speed_in_y
+	dst = cv2.warpAffine(frame, M, (output_wd,output_ht))
 	out.write(dst)
-	cv2.imshow('window-name',dst)
+	#cv2.imshow('window-name',dst)
 	#cv2.imwrite("frame%d.jpg" % count, frame)
 	#count = count + 1
-	
-
-
-
-
-
-	if cv2.waitKey(10) & 0xFF == ord('q'):
-		break
+	#if cv2.waitKey(10) & 0xFF == ord('q'):
+	#	break
 
 
 #while(cap.isOpened()):
@@ -101,11 +84,10 @@ for num in range (start_frame,end_frame):
 #track_window = (c,r,w,h)
 
 
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
 cap.release()
 out.release()
 
 #cap.release()
 #cv2.destroyAllWindows()
 
-print "end"
