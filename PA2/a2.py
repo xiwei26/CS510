cda3cfd51d6df14b1719f7c25d48e24b6671ed53
Zhixian Yu <Zhixian Yu>
2017-02-15 14:41:39 -0700

    print key point number

--- PA2/a2.py
+++ PA2/a2.py
@@ -39,15 +39,16 @@ def kp_detect(frame, coordinatesList, hessian):
 		surf = cv2.SURF(float(hessian))
 		kp = surf.detect(gray,None)
 		total_kp = len(kp)
-		if total_kp > 30:
+		if total_kp > 100:
 			break
 		else:
 			hessian -= 1000
-
+	#print "-------------------"
 	for a in range(total_kp):
 		x = int(kp[a].pt[0])
 		y = int(kp[a].pt[1])
 		diameter = kp[a].size
+		#print (a, x,y,diameter)
 		# check if (x,y) already in coordinates list
 		if (x,y,diameter) not in coordinatesList:
 			for b in coordinatesList:
@@ -61,6 +62,7 @@ def kp_detect(frame, coordinatesList, hessian):
 					coordinatesList.pop(0)
 					coordinatesList.append((x,y,diameter))
 					break
+	#print "-------------------"
 	return (total_kp, hessian)
 
 #####input file name#####
@@ -83,16 +85,17 @@ if cap.isOpened()==False:
 total_frame_num = 50
 #####processing video#####
 coordinatesList = []
-hessian = 20000
+hessian = 5000
 
 if not isGaussian:
 	for i in range (total_frame_num):
 
 		ret, frame = cap.read()
-		hessian = kp_detect(frame, coordinatesList, hessian)
+		total_kp, hessian = kp_detect(frame, coordinatesList, hessian)
 
 		#print "coordinate len: ",len(coordinates)
 		print "Frame#",i,",keypoint:", coordinatesList[-1]
+		#print total_kp
 		writeKP(frame, coordinatesList[-1][0], coordinatesList[-1][1], coordinatesList[-1][2], './result/',i)
 
 		if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -100,28 +103,29 @@ if not isGaussian:
 else:
 	avg_kp_size = []
 	largest_kp_size = []
-	for ksize in range(3, 11, 2):#[11]: #
-		hessian = 20000
+	for ksize in [15]:#range(3, 11, 2):#[11]: #
+		hessian = 5000
 		print 'ksize is %d' %ksize
 		isWriting = raw_input("Are you going to write images labeled with key points (y or n): ")
 		isWriting = True if isWriting == 'y' else False
 		sum_sz = 0 ### for calculating the average key point size
 		large_sz = 0 ### for calculating the largest key point size
+
 		for i in range (total_frame_num):
 			ret, frame = cap.read()
 			frame_gaus = cv2.GaussianBlur(frame, (ksize, ksize), 0)
 			(total_kp, hessian) = kp_detect(frame_gaus, coordinatesList, hessian)
 			if isWriting:
+				print "Frame#",i,",keypoint:", coordinatesList[-1]
 				writeKP(frame, coordinatesList[-1][0], coordinatesList[-1][1], coordinatesList[-1][2], './result_var/',i)
+				print total_kp
 			sz = coordinatesList[-1][2]
 			sum_sz += sz
 			large_sz = sz if sz > large_sz else large_sz
 
-
-
 		avg_kp_size.append(sum_sz / total_frame_num)
 		largest_kp_size.append(large_sz)
-	print 'ksize: ', [range(3, ksize, 2)] 
+	print 'ksize: ', [range(3, ksize+1, 2)] 
 	print 'average', avg_kp_size
 	print 'largest', largest_kp_size
 
