29fb70155bf54c156d33b5cb03c637a084bf675a
Wei Xi <xiweiboy@gmail.com>
2017-03-20 14:39:24 -0600

    Update A3.py

--- A3/A3.py
+++ A3/A3.py
@@ -1,46 +1,64 @@
 import sys
-sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
 import numpy as np
-
 import scipy
 from scipy import ndimage
 
-##### input file name #####
+##### read input file #####
 if len(sys.argv) != 2:
-	print "Usage: python a1.py <in_file>"
+	print "Usage: python a3.py <in_file>"
 	sys.exit()
-in_file=sys.argv[1]
 
-##### read video information #####
+in_file = sys.argv[1]
 cap = cv2.VideoCapture(in_file)
-if cap.isOpened()==False:
+if cap.isOpened() == False:
 	print("Can not open the video")
 	sys.exit()
-total_frame_num = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-#total_frame_num = 50
+total_frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+out_file = open("resultR", 'w')
 ### Background substraction ###
-fgbg = cv2.BackgroundSubtractorMOG()
-
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 n=10
 l=256
 kernel = np.ones((5,5),np.uint8)
-
+information = []
 for i in range(total_frame_num):
 	ret, frame = cap.read()
-	fgmask = fgbg.apply(frame)
+	if(i == 0):
+		fgmask = fgbg.apply(frame)
+	else:
+		fgbg.apply(frame, fgmask, -1)
+
+
 	#opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 	#closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-	ret, thresh = cv2.threshold(closing, 127, 255, 0)
-	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	for a in contours:
-    # Approximates rectangles
-		x,y,w,h = cv2.boundingRect(a)
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+	
+	output = cv2.connectedComponentsWithStats(fgmask, 8, cv2.CV_32S);
+	num_labels = output[0]
+	stats = output[2]
+	index =0 
+	for j in range(num_labels):
+		x = stats[j, cv2.CC_STAT_LEFT]
+		y = stats[j, cv2.CC_STAT_TOP]
+		w = stats[j, cv2.CC_STAT_WIDTH]
+		h = stats[j, cv2.CC_STAT_HEIGHT]
+		area = stats[j, cv2.CC_STAT_AREA]
+		
+		lr_x=x+w;
+		lr_y=y+h;
+		if(w > 22 and w < 500):
+			if(h > 40):
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+				if(i==1):
+					out_file.write(str(index)+","+str(i)+","+str(x)+","+str(y)+","+str(lr_x)+","+str(lr_y)+"\n")
+					information.append(index,i,x,y,lr_x,lr_y)
+					index=index+1
+				else:
+					#start tracking
 
-
-
+				print w,h,area
+	print i
 
 	filename = './result/' + str(i) + '.png'
-	cv2.imwrite(filename,closing)
+	cv2.imwrite(filename,frame)
