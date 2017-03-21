f03522fdde1c6db85dabf6b564cc122f489de893
Zhixian Yu <Zhixian Yu>
2017-03-21 11:54:45 -0600

    modify pseudocode.py

--- PA3/pseudocode.py
+++ PA3/pseudocode.py
@@ -2,8 +2,9 @@ trackers = []
 tracking_objects = []
 for each frame:
 	for i in range(len(trackers)):
-		t.update(frame)
-		tracking_objects[i].append((object_x, object_y, object_width, object_height, psr))
+		t.update(frame)  #for each tracker (i.e. each template), see if we can track anything
+		if t.good:  #if the object can be tracked, MOSSE class has member variable called self.good
+			tracking_objects[i].append((object_x, object_y, object_width, object_height, frame_number, t.psr)) 
 	moving_objects = [find_moving_objects()] # mix of gaussian to identify moving objects, eliminating small objects
 	new_objects = []
 	for obj in moving_objects:
@@ -12,7 +13,7 @@ for each frame:
 	for new_obj in new_objects:  
 		new_track = MOSSE(frame, new_obj)  # create new tracker for each new object
 		trackers.append(new_track)
-		tracking_objects.append([(object_x, object_y, object_width, object_height, psr)])  #for each new object, record its position
+		tracking_objects.append([(object_x, object_y, object_width, object_height, frame_number, new_track.psr)])  #for each new object, record its position
 
 f = open('result.csv', 'w')
 f.write(tracking_objects)
